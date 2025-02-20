f8cce7b2ffb8b2f48cd99272bbb449d3cfd62147
Mohye Elsayed <Mohye20@gmail.com>
2025-02-20 15:52:32 +0200

    Server Side Validations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -28,9 +28,14 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _dbContext.Categories.Add(obj);
-            _dbContext.SaveChanges();
-            return RedirectToAction("Index");
+           if(ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
